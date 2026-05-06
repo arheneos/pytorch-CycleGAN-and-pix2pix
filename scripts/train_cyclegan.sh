f887d74b4ae0ecdf8d4e9c6607a7c9df52d23eee
arheneos <arheneos@snu.ac.kr>
2026-05-06 13:26:46 +0900

    a

--- scripts/train_cyclegan.sh
+++ scripts/train_cyclegan.sh
@@ -1,3 +1,3 @@
 set -ex
-torchrun --nproc_per_node=2 train.py --dataroot ./datasets/maps --name maps_cyclegan --model cycle_gan --pool_size 50 --no_dropout  --use_wandb
+torchrun --nproc_per_node=2 train.py --dataroot ./datasets/maps --name maps_cyclegan --model cycle_gan --pool_size 50 --no_dropout --norm sync_batch  --use_wandb
 ```
