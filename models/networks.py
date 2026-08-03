dc917ce7f9b41a5edd2e7fb659b87c8229514240
arheneos <arheneos@snu.ac.kr>
2026-08-03 13:10:29 +0900

    abb

--- models/networks.py
+++ models/networks.py
@@ -2187,7 +2187,6 @@ class NLayerDiscriminator(nn.Module):
             use_bias = norm_layer.func == nn.InstanceNorm2d
         else:
             use_bias = norm_layer == nn.InstanceNorm2d
-
         kw = 4
         padw = 1
         sequence = [nn.Conv2d(input_nc, ndf, kernel_size=kw, stride=2, padding=padw), nn.LeakyReLU(0.2, True)]
